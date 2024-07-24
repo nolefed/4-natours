2b162931681e0a86112615252b837c97e48cc832
manish bhusal <manishbhusal@gmail.com>
2024-07-24 11:13:16 -0400

    Handling Mongoose Validation Errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,79 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err =>{
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err =>{
+    
+
+    const value=err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+    console.log(value);
+    const message = ` Duplicate field value: ${value}. Please use another value!`;
+    return new AppError(message,400);
+};
+
+const handleValidationErrorDB = err => {
+    const errors = Object.values(err.errors).map(el=>el.message);
+
+    const message = `Invalid input Data. ${errors.join('. ')}`;
+    return new AppError(message,400);
+};
+
+const sendErrorDev = (err,res) => {
+    res.status(err.statusCode).json({
+        satus:err.status,
+        error:err,
+        message:err.message,
+        stack:err.stack
+    });
+};
+const sendErrorProd = (err,res) =>{
+    // Operational, trusted error : send message to client
+    if(err.isOperational){
+        res.status(err.statusCode).json({
+            satus:err.status,
+            message:err.message,
+            
+        });
+     // programming or other unknown error: do not leak error details   
+    }else{
+
+        // 1) LOG ERROR
+        console.error('ERROR ', err);
+
+        // 2) SEND GENERIC MESSAGE
+        res.status(500).json({
+            status: 'error',
+            message: 'something went wrong!'
+        })
+    }
+   
+};
+
+
 module.exports= (err,req,res,next) => {
     //console.log(err.stack);
     err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error'
-    res.status(err.statusCode).json({
-        sattus:err.status,
-        message:err.message
-    });
+    err.status = err.status || 'error';
+ 
+    if(process.env.NODE_ENV === 'development'){
+        sendErrorDev(err,res);        
+    }else if(process.env.NODE_ENV === 'production'){ 
+        let error = {...err};
+        if(error.name === 'CastError') {
+           error= handleCastErrorDB(error);
+        }
+        if(error.code === 11000){
+            error=handleDuplicateFieldsDB(error);
+        }
+        if(error.code === 'ValidationError'){
+            error=handleValidationErrorDB(error);
+        }
+            
+        sendErrorProd(error,res);
+    }
+
+    
 };    
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,6 +2,7 @@ const { Query } = require('mongoose');
 const Tour = require('./../models/tourmodel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.aliasTopTours = (req,res,next) => {
     req.query.limit= '5';
@@ -29,6 +30,11 @@ exports.getAllTours = catchAsync(async(req,res,next) => {
 exports.getTour = catchAsync(async(req,res,next) => {
     
     const tour = await Tour.findById(req.params.id);
+
+    if(!tour){
+        return next(new AppError ('No tour found with that ID', 404));
+    }
+
     res.status(200).json({
         status: 'success',
             data: {
@@ -55,6 +61,11 @@ exports.updateTour = catchAsync(async (req,res,next) =>{
         new:true,
         runValidators:true
     })
+
+    if(!tour){
+        return next(new AppError ('No tour found with that ID', 404));
+    }
+
     res.status(200).json({
         status: 'success',
         data: {
@@ -65,7 +76,12 @@ exports.updateTour = catchAsync(async (req,res,next) =>{
 
 exports.deleteTour = catchAsync(async (req,res,next) =>{
 
-    await Tour.findByIdAndDelete(req.params.id);
+   const tour= await Tour.findByIdAndDelete(req.params.id);
+
+    if(!tour){
+        return next(new AppError ('No tour found with that ID', 404));
+    } 
+
     res.status(204).json({
         status: 'success',
         data: null
