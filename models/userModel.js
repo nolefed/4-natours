df72710e8ffeac4fa88d20db4df17e5c5172b7ec
manish bhusal <manishbhusal@gmail.com>
2024-07-27 13:05:06 -0400

    modelling users

---
+++ models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true,'Please provide your name!']
+    },
+    email:{
+        type:String,
+        required: [true,'Please provide your email'],
+        unique:true,
+        lowercase:true,
+        validator:[validator.isEmail]
+    },
+    photo:String,
+    password:{
+        type:String,
+        required: [true,'Please provide a password'],
+        minlength:8
+    },
+    passwordConfirm:{
+        type:String,
+        required: [true,'Please confirm your password']     
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports=User;
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err =>{
+    console.log('UNCAUGHT EXCEPTION! Shutting Down');
+    console.log(err.name, err.message);
+    process.exit(1); 
+    
+});
+
 dotenv.config({path:'./config.env'});
 const app = require('./app');
 
@@ -30,13 +37,7 @@ process.on('unhandledRejection', err =>{
 });
 
 
-process.on('uncaughtException', err =>{
-    console.log('UNCAUGHT EXCEPTION! Shutting Down');
-    console.log(err.name, err.message);
-    server.close(()=>{
-        process.exit(1); 
-    })
-})
+
 
 
 
