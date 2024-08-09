5f69b6ab1e10605b284ff8ae9173436f3d6f57fc
manish bhusal <manishbhusal@gmail.com>
2024-08-09 08:43:00 -0400

    Data sanitization

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -11,11 +14,15 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //1)  GLOBAL MIDDLEWARES
+//    Set security HTTP headers
+app.use(helmet());
 
+// Development logging
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
     max: 100,
     windowMs: 60*60*1000,
@@ -24,9 +31,19 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json());
+//Body parser, reading data from the body into req.body
+app.use(express.json({ limit:'10kb' }));
+
+// Data Sanitization against NOSQL query injection
+app.use(mongoSanitize());
+
+// Data Sanitization against  XSS
+app.use(xss());
+
+//Serving static files
 app.use(express.static(`${__dirname}/starter/public`));
 
+// Test Middleware
 app.use((req,res,next) =>{
     req.requestTime = new Date().toISOString();
     next();
