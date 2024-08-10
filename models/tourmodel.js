5e1a8648261ba951d56b46219978913892490902
manish bhusal <manishbhusal@gmail.com>
2024-08-10 10:23:04 -0400

    modelling reviews

--- models/tourmodel.js
+++ models/tourmodel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 const validator = require ('validator');
 
 const tourSchema = new mongoose.Schema({
@@ -101,6 +102,12 @@ const tourSchema = new mongoose.Schema({
             description: String,
             day: Number
         }
+    ],
+    guides: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User'
+        }
     ]
 },
     {
@@ -119,6 +126,12 @@ tourSchema.pre('save', function(next){
     next();
 })
 
+// tourSchema.pre('save', async function(next){
+//     const guidesPromises = this.guides.map(async id=> await User.findById(id));
+//     this.guides = await Promise.all(guidesPromises);
+//     next();
+// });
+
 // tourSchema.save('save', function(next){
 //     console.log("will save document");
 //     next();
@@ -136,10 +149,21 @@ tourSchema.pre('/^find/', function(next){
     next();
 });
 
+tourSchema.pre(/^find/, function(next) {
+    this.populate({
+        path:'guides',
+        select: '-__v -passwordChangedAt'
+    });
+    next();
+});
+
 tourSchema.post(/^find/, function(docs,next){
     console.log(`Query took ${Date.now() - this.start} milliseconds!`);
     next();
-})
+});
+
+
+
 
 //AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function(next){
