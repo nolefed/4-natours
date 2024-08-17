f644ffe4e9790a4597205cfaba155fea56d9fea0
manish bhusal <manishbhusal@gmail.com>
2024-08-17 07:12:20 -0400

    adding a nested get endpoint

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController.js');
 const authController = require('./../controllers/authController');
-const reviewController = require('./../controllers/reviewController.js');
+const reviewRouter = require('./../routes/reviewRoutes.js');
+
 const router = express.Router();
 
 //router.param('id', tourController.checkID);
+
+//POST/tour/234fad4/reviews
+//GET/tour/234fad4/reviews
+
+
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router.route('/top-5-cheap/').get(tourController.aliasTopTours, tourController.getAllTours);
 
@@ -22,10 +30,6 @@ router
     .patch(tourController.updateTour)
     .delete(authController.protect,authController.restrictTo('admin','lead-guide'), tourController.deleteTour);
 
-//POST/tour/234fad4/reviews
-//GET/tour/234fad4/reviews
-//GET/tour/234fad4/reviews/92342342
-
-router.route('/:tourId/reviews').post(authController.protect,authController.restrictTo('user'),reviewController.createReview);    
+   
 
 module.exports= router;
