839ac7e8db4096cde8883ae59ea942e78a7e18b9
manish bhusal <manishbhusal@gmail.com>
2024-08-21 12:11:14 -0400

    added missing authorization and authentication

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -8,13 +8,16 @@ const router = express.Router({ mergeParams:true });
 //POST/tour/234fad4/reviews
 //POST/reviews
 
+router.use(authController.protect);
 
 router.route('/')
       .get(reviewController.getAllReviews)
-      .post(authController.protect, authController.restrictTo('user'),reviewController.setTourUserIds,reviewController.createReview);
+      .post(authController.restrictTo('user'),reviewController.setTourUserIds,reviewController.createReview);
 
 router.route('/:id')
-.get(reviewController.getReview).patch(reviewController.updateReview).delete(reviewController.deleteReview);
+.get(reviewController.getReview)
+.patch(authController.restrictTo('user','admin'), reviewController.updateReview)
+.delete(authController.restrictTo('user','admin'),reviewController.deleteReview);
 
 module.exports=router;
  
